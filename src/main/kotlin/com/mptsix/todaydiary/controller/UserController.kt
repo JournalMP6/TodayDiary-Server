c01c5404078d71c1aaaa401b19998ae61741f901
KangDroid <hyunwoo286@gmail.com>
2021-05-25 19:54:21 +0900

    [Controller] Add Endpoint and hook up registering journal

--- src/main/kotlin/com/mptsix/todaydiary/controller/UserController.kt
+++ src/main/kotlin/com/mptsix/todaydiary/controller/UserController.kt
@@ -2,13 +2,17 @@ package com.mptsix.todaydiary.controller
 
 import com.mptsix.todaydiary.data.request.LoginRequest
 import com.mptsix.todaydiary.data.request.UserRegisterRequest
+import com.mptsix.todaydiary.data.response.JournalResponse
 import com.mptsix.todaydiary.data.response.LoginResponse
 import com.mptsix.todaydiary.data.response.UserRegisterResponse
+import com.mptsix.todaydiary.data.user.journal.Journal
 import com.mptsix.todaydiary.service.UserService
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -27,5 +31,13 @@ class UserController(
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(userService.loginUser(loginRequest))
+    }
+
+    @PostMapping("/api/v1/journal")
+    fun registerJournal(@RequestHeader header: HttpHeaders, @RequestBody journal: Journal): ResponseEntity<JournalResponse> {
+        val userToken: String = header["X-AUTH-TOKEN"]!![0]
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(userService.registerJournal(userToken, journal))
     }
 }
--- src/test/kotlin/com/mptsix/todaydiary/controller/UserControllerTest.kt
+++ src/test/kotlin/com/mptsix/todaydiary/controller/UserControllerTest.kt
@@ -3,8 +3,11 @@ package com.mptsix.todaydiary.controller
 import com.mptsix.todaydiary.data.user.User
 import com.mptsix.todaydiary.data.request.LoginRequest
 import com.mptsix.todaydiary.data.request.UserRegisterRequest
+import com.mptsix.todaydiary.data.response.JournalResponse
 import com.mptsix.todaydiary.data.response.LoginResponse
 import com.mptsix.todaydiary.data.response.UserRegisterResponse
+import com.mptsix.todaydiary.data.user.journal.Journal
+import com.mptsix.todaydiary.service.UserService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -13,13 +16,13 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.exchange
 import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.remove
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,6 +34,9 @@ internal class UserControllerTest {
     @Autowired
     private lateinit var mongoTemplate: MongoTemplate
 
+    @Autowired
+    private lateinit var userService: UserService
+
     private val restTemplate: TestRestTemplate = TestRestTemplate()
 
     private var serverUrl: String = "http://localhost:$port"
@@ -40,6 +46,25 @@ internal class UserControllerTest {
     fun clearDb() {
         serverUrl = "http://localhost:$port"
         mongoTemplate.remove<User>(Query())
+    }
+
+    fun loginUser(): String {
+        val userRegisterRequest: UserRegisterRequest = UserRegisterRequest(
+            userId = "KangDroid",
+            userPassword = "test",
+            userName = "KDR",
+            userDateOfBirth = "WHENEVER",
+            userPasswordAnswer = "WHAT",
+            userPasswordQuestion = "WHAT"
+        )
+        userService.registerUser(userRegisterRequest)
+
+        return userService.loginUser(
+            LoginRequest(
+                userId = userRegisterRequest.userId,
+                userPassword = userRegisterRequest.userPassword
+            )
+        ).userToken
     }
 
     @Test
@@ -83,6 +108,29 @@ internal class UserControllerTest {
         assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(responseEntity.hasBody()).isEqualTo(true)
         assertThat(responseEntity.body!!.userToken).isNotEqualTo("")
+    }
+
+    @Test
+    fun is_registerJournal_works_well() {
+        val mockJournal: Journal = Journal(
+            mainJournalContent = "Today was great!",
+            journalLocation = "Somewhere over the rainbow!",
+            journalCategory = "Somewhat_category",
+            journalWeather = "Sunny",
+            journalDate = System.currentTimeMillis()
+        )
+        val userToken: String = loginUser()
+        val httpHeaders: HttpHeaders = HttpHeaders().apply {
+            put("X-AUTH-TOKEN", listOf(userToken))
+        }
+        val journalResponse: ResponseEntity<JournalResponse> =
+            restTemplate.exchange("${serverUrl}/api/v1/journal", HttpMethod.POST, HttpEntity(mockJournal, httpHeaders))
+
+        assertThat(journalResponse.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(journalResponse.hasBody()).isEqualTo(true)
+        assertThat(journalResponse.body).isNotEqualTo(null)
+        assertThat(journalResponse.body!!.registeredJournal.mainJournalContent)
+            .isEqualTo(mockJournal.mainJournalContent)
     }
 
 }
